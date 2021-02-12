ae51f1b358a207b1ca7713ffd6413e1392d780aa
jackhowa <jack.howard@washpost.com>
2021-02-12 16:48:20 -0600

    Basic redux like setup per https://github.com/kentcdodds/react-performance/blob/main/src/final/06.js

--- src/App.js
+++ src/App.js
@@ -2,22 +2,6 @@ import * as React from 'react'
 import './vars.css'
 import styled from 'styled-components'
 
-const ThemeContainer = styled.div({
-  padding: 20,
-  backgroundColor: 'var(--colors-secondary)',
-  // for light/dark mode of the color of text
-  color: '#fff'
-})
-
-const PrimaryTextContainer = styled.div({
-  backgroundColor: 'var(--colors-primary)',
-})
-
-const PrimaryText = styled.h1({
-  fontSize: "10vw",
-  textAlign: 'center',
-})
-
 const SUPER_HERO_OPTIONS = [
   {
     id: 'iron-man',
@@ -29,12 +13,84 @@ const SUPER_HERO_OPTIONS = [
   }
 ]
 
-function ThemeToggler({ setSuperHeroID }) {
+const AppStateContext = React.createContext()
+const AppDispatchContext = React.createContext()
+
+function appReducer(state, action) {
+  console.log(state, action)
+  switch (action.type) {
+    case 'UPDATE_SUPER_HERO': {
+      return { ...state, superHeroID: action.id }
+    }
+    default: {
+      throw new Error(`Unhandled action type: ${action.type}`)
+    }
+  }
+}
+
+function AppProvider({ children }) {
+  const [state, dispatch] = React.useReducer(appReducer, {
+    superHeroID: 'iron-man',
+  })
+  console.log(state, 'initial state')
+  return (
+    <AppStateContext.Provider value={state}>
+      <AppDispatchContext.Provider value={dispatch}>
+        {children}
+      </AppDispatchContext.Provider>
+    </AppStateContext.Provider>
+  )
+}
+
+
+function useAppState() {
+  const context = React.useContext(AppStateContext)
+  if (!context) {
+    throw new Error('useAppState must be used within the AppProvider')
+  }
+  return context
+}
+
+function useAppDispatch() {
+  const context = React.useContext(AppDispatchContext)
+  if (!context) {
+    throw new Error('useAppDispatch must be used within the AppProvider')
+  }
+  return context
+}
+
+let ThemeContainer = styled.div({
+  padding: 20,
+  backgroundColor: 'var(--colors-secondary)',
+  // for light/dark mode of the color of text
+  color: '#fff'
+})
+
+ThemeContainer = React.memo(ThemeContainer)
+
+const PrimaryTextContainer = styled.div({
+  backgroundColor: 'var(--colors-primary)',
+})
+
+const PrimaryText = styled.h1({
+  fontSize: "10vw",
+  textAlign: 'center',
+})
+
+function ThemeToggler() {
+  const dispatch = useAppDispatch()
+
+  console.log(dispatch)
+
+
   // should not update each change of pick
   return (
     SUPER_HERO_OPTIONS.map(({ id, headline }) => {
       return (
-        <button key={id} onClick={() => setSuperHeroID(id)}>
+        <button key={id} onClick={() => dispatch({
+          type: 'UPDATE_SUPER_HERO',
+          id
+        })}>
           Change to {headline} mode
         </button>
       )
@@ -42,7 +98,12 @@ function ThemeToggler({ setSuperHeroID }) {
   )
 }
 
-function SuperHeroDisplayText({ targetSuperHeroID }) {
+function SuperHeroDisplayText() {
+  const state = useAppState()
+  console.log(state, 'all state')
+  const targetSuperHeroID = state.superHeroID
+  console.log(targetSuperHeroID)
+
   React.useEffect(() => {
     document.body.dataset.theme = targetSuperHeroID
   }, [targetSuperHeroID])
@@ -60,13 +121,14 @@ function SuperHeroDisplayText({ targetSuperHeroID }) {
 }
 
 function App() {
-  const [targetSuperHeroID, setSuperHeroID] = React.useState('iron-man')
-
   return (
-    <div className="App">
-      <SuperHeroDisplayText targetSuperHeroID={targetSuperHeroID} />
-      <ThemeToggler setSuperHeroID={setSuperHeroID} />
-    </div>
+    <AppProvider>
+      <div className="App">
+        <SuperHeroDisplayText />
+        <ThemeToggler />
+      </div>
+    </AppProvider>
+
   );
 }
 
